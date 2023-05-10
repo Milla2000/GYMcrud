650a4fdafadba7ffa10377c04a7b448bd99aa61f
Milla2000 <millajesso2000@gmail.com>
2023-05-10 23:25:50 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateRegistrationComponent } from './create-registration/create-registration.component';
+import { RegistrationListComponent } from './registration-list/registration-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { NgConfirmModule } from 'ng-confirm-box';
+import { NgToastModule } from 'ng-angular-popup';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRegistrationComponent,
+    RegistrationListComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    NgToastModule,
+    NgConfirmModule,
+
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
